1638de3feec6a822af5530be99cd6cd65868c5d1
Abhishek Patil <abishkpatil@gmail.com>
2024-08-30 17:38:39 +0530

    feat: testing dynamic endpoint

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -26,7 +26,14 @@ const Settings = ({ user }) => {
                     username: user.email.split("@")[0],
                     appType: appType
                 };
-                const response = await axios.post(`https://ec0a52f5-b2bf-4b82-8567-1490689ccb58-00-3n71ekg2avu7v.sisko.replit.dev/checkconnection`, data, {
+                // const response = await axios.post(`https://ec0a52f5-b2bf-4b82-8567-1490689ccb58-00-3n71ekg2avu7v.sisko.replit.dev/checkconnection`, data, {
+                //     headers: {
+                //         'Authorization': `Bearer ${idToken}`,
+                //         'Content-Type': 'application/json'
+                //     }
+                // });
+                const checkconnectionURL = import.meta.env.VITE_BACKEND_URL + "/checkconnection"
+                const response = await axios.post(checkconnectionURL, data, {
                     headers: {
                         'Authorization': `Bearer ${idToken}`,
                         'Content-Type': 'application/json'
@@ -62,7 +69,14 @@ const Settings = ({ user }) => {
             const data = {
                 username: user.email.split("@")[0],
             };
-            const response = await axios.post(`https://ec0a52f5-b2bf-4b82-8567-1490689ccb58-00-3n71ekg2avu7v.sisko.replit.dev/enabletrigger`, data, {
+            // const response = await axios.post(`https://ec0a52f5-b2bf-4b82-8567-1490689ccb58-00-3n71ekg2avu7v.sisko.replit.dev/enabletrigger`, data, {
+            //     headers: {
+            //         'Authorization': `Bearer ${idToken}`,
+            //         'Content-Type': 'application/json'
+            //     }
+            // });
+            const enableTriggerURL = import.meta.env.VITE_BACKEND_URL + "/enabletrigger"
+            const response = await axios.post(enableTriggerURL, data, {
                 headers: {
                     'Authorization': `Bearer ${idToken}`,
                     'Content-Type': 'application/json'
@@ -87,7 +101,14 @@ const Settings = ({ user }) => {
                 username: user.email.split("@")[0],
                 appType: appType
             };
-            const response = await axios.post(`https://ec0a52f5-b2bf-4b82-8567-1490689ccb58-00-3n71ekg2avu7v.sisko.replit.dev/newentity`, data, {
+            // const response = await axios.post(`https://ec0a52f5-b2bf-4b82-8567-1490689ccb58-00-3n71ekg2avu7v.sisko.replit.dev/newentity`, data, {
+            //     headers: {
+            //         'Authorization': `Bearer ${idToken}`,
+            //         'Content-Type': 'application/json'
+            //     }
+            // });
+            const newEntityURL = import.meta.env.VITE_BACKEND_URL + "/newentity"
+            const response = await axios.post(newEntityURL, data, {
                 headers: {
                     'Authorization': `Bearer ${idToken}`,
                     'Content-Type': 'application/json'
